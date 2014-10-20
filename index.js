d3d443466bff172eb09d40fbe008768cc99cef21
Sven Paulus <sven@karlsruhe.org>
2014-10-20 16:31:44 +0200

    merging https://github.com/xjamundx/jshint-junit-reporter/blob/master/reporter.js and https://github.com/spenceralger/gulp-jshint-file-reporter/blob/master/index.js

--- index.js
+++ index.js
@@ -5,6 +5,41 @@ var defaultFilename = 'jshint-output.log';
 var wrStream;
 var filename;
 
+function encode(s) {
+  var pairs = {
+    "&": "&amp;",
+    '"': "&quot;",
+    "'": "&apos;",
+    "<": "&lt;",
+    ">": "&gt;"
+  };
+  for (var r in pairs) {
+    if (typeof(s) !== "undefined") {
+      s = s.replace(new RegExp(r, "g"), pairs[r]);
+    }
+  }
+  return s || "";
+}
+
+function failure_message(failures) {
+  var count = failures.length;
+  if (count === 1) {
+    return "1 JSHINT Failure";
+  } else {
+    return count + " JSHint Failures";
+  }
+}
+
+function failure_details(failures) {
+  var msg = [];
+  var item;
+  for (var i = 0; i < failures.length; i++) {
+    item = failures[i];
+    msg.push(i+1 + ". line " + item.line + ", char " + item.character + ": " + encode(item.reason));
+  }
+  return msg.join("\n");
+}
+
 module.exports = function (results, data, opts) {
   opts = opts || {};
   opts.filename = opts.filename || defaultFilename;
@@ -20,18 +55,34 @@ module.exports = function (results, data, opts) {
   }
 
   var out = [];
-  var file = {};
+  var files = {};
 
-  results.forEach(function (result, i) {
-    var err = result.error;
-
-    if (i === 0) {
-      // start off the output with the filename
-      out.push(results.length + ' lint errors found in ' + result.file);
+  results.forEach(function (result) {
+    result.file = result.file.replace(/^\.\//, '');
+    if (!files[result.file]) {
+      files[result.file] = [];
     }
-
-    out.push('  [' + err.line + ',' + err.character + '](' + err.code + ') ' + err.reason);
+    files[result.file].push(result.error);
   });
 
-  wrStream.write(out.join('\n'));
+  out.push("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+  out.push("<testsuite name=\"" + suite + "\" tests=\"" + (Object.keys(files).length === 0 ? 1 : Object.keys(files).length) + "\" failures=\"" + results.length + "\" errors=\"0\">");
+
+  // we need at least 1 empty test
+  if (!results.length) {
+    out.push("\t<testcase name=\"" + suite + "\" />");
+  }
+
+  for (var file in files) {
+    out.push("\t<testcase name=\"" + file + "\">");
+    out.push("\t\t<failure message=\"" + failure_message(files[file]) + "\">");
+    out.push(failure_details(files[file]));
+    out.push("\t\t</failure>");
+    out.push("\t</testcase>");
+  }
+
+  out.push("</testsuite>");
+  out = out.join("\n") + "\n";
+
+  wrStream.write(out);
 };
